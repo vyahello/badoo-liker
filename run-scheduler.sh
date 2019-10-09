dbf24d5435f7f2c3b9f79868a543236d6492a977
Volodymyr Yahello <vyahello@gmail.com>
2019-10-09 20:00:13 +0300

    Add infinite scheduler runner

--- run-scheduler.sh
+++ run-scheduler.sh
@@ -3,14 +3,24 @@
 # pre-setup configuration, please set it to desired values
 CONFIG="setup.yaml"
 RUNS=10
-DELAY_BETWEEN_RUN=3600
+DELAY_BETWEEN_RUN=60
+LOGS="logs.txt"
+
+
+function run-delayed-liker {
+   python liker.py --config ${CONFIG}
+   sleep ${DELAY_BETWEEN_RUN}
+}
 
 
 function do-executor {
-    for (( run=1; run<=${RUNS}; run++ )); do
-      python liker.py --config ${CONFIG}
-    sleep ${DELAY_BETWEEN_RUN}
+    iteration=0
+    while true; do
+        echo "Running #$iteration iteration ..."
+        run-delayed-liker
+        let iteration+=1
     done
+    echo "Something went wrong, please see $LOGS for more details!"
 }
 
 
